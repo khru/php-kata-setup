7d7ff402ae5c0a62ef34c81868a5ebf2b0e513f0
katarn <katarn@katarn.es>
2023-11-11 17:57:57 +0100

    add test return type

--- tests/ChangeMeTest.php
+++ tests/ChangeMeTest.php
@@ -8,7 +8,8 @@ use PHPUnit\Framework\TestCase;
 
 class ChangeMeTest extends TestCase
 {
-    function testChangeMe() {
+    function testChangeMe(): void
+    {
         $changeMe = new ChangeMe();
         self::assertSame(true, $changeMe->change());
     }
